f2b2b3385d37149a910bb8faa46b21f220338816
Ernando <42004366+devjva@users.noreply.github.com>
2021-10-27 11:57:05 -0400

    teste

--- src/ios/Linphone.h
+++ src/ios/Linphone.h
@@ -1,29 +1,12 @@
-#import <Cordova/CDV.h>
 #import <AudioToolbox/AudioToolbox.h>
+//#include "linphone/lpconfig.h"
 #include "linphone/linphonecore.h"
+#include "linphone/linphonecore_utils.h"
 
-@interface Linphone : CDVPlugin{
-    LinphoneCore *lc;
-    LinphoneCall *call;
-}
-
+//  RCTCalendarModule.h
+#import <React/RCTBridgeModule.h>
+@interface SipModule : NSObject <RCTBridgeModule>
 @property (nonatomic) LinphoneCore *lc;
 @property (nonatomic) LinphoneCall *call;
-
-- (void)acceptCall:(CDVInvokedUrlCommand*)command;
-- (void)listenCall:(CDVInvokedUrlCommand*)command;
-- (void)login:(CDVInvokedUrlCommand*)command;
-- (void)logout:(CDVInvokedUrlCommand*)command;
-- (void)call:(CDVInvokedUrlCommand*)command;
-- (void)videocall:(CDVInvokedUrlCommand*)command;
-- (void)hangup:(CDVInvokedUrlCommand*)command;
-- (void)toggleVideo:(CDVInvokedUrlCommand*)command;
-- (void)toggleSpeaker:(CDVInvokedUrlCommand*)command;
-- (void)toggleMute:(CDVInvokedUrlCommand*)command;
-- (void)sendDtmf:(CDVInvokedUrlCommand*)command;
-- (void)setLowBandwidth:(CDVInvokedUrlCommand*)command;
-- (void)updateRegister:(CDVInvokedUrlCommand*)command;
-- (void)getRemoteContact:(CDVInvokedUrlCommand*)command;
-
-
 @end
+
